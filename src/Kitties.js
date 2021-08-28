107d770f6733fbbd63e1dcd87b87a8ec18a5bc42
abel <raorolli@gmail.com>
2021-08-28 23:28:21 +0800

    update

--- src/Kitties.js
+++ src/Kitties.js
@@ -23,22 +23,31 @@ export default function Kitties (props) {
     //   - 每只猫咪的主人是谁
     //   - 每只猫咪的 DNA 是什么，用来组合出它的形态
 
-    api.query.kittiesModule.kittiesCount().then(value => {
-      for (let index = 0; index < value; index++) {
+    api.query.kittiesModule.kittiesCount().then(currentIndex => {
+      for (let index = 0; index <= currentIndex; index++) {
         let kittyDna = ''
         let kittyOwner = ''
         api.query.kittiesModule.kittiesIdMap(index).then(value => {
-
           kittyDna = JSON.stringify(value)
           api.query.kittiesModule.kittiesOwnerMap(index).then(value => {
             kittyOwner = JSON.stringify(value)
             console.log('kitty info: ', index, kittyDna, kittyOwner)
-            kitties.push({id:index,dna:kittyDna,owner:kittyOwner});
+            kitties.push({
+              id: index,
+              dna: kittyDna,
+              owner: kittyOwner
+            })
+            if (index == currentIndex) {
+              console.log('end')
+              setKitties(kitties)
+            }
           })
         })
-        setKitties(kitties)
+
       }
     })
+
+    console.log('fetch info')
 
   }
 
@@ -54,6 +63,7 @@ export default function Kitties (props) {
     // 这个 kitties 会传入 <KittyCards/> 然后对每只猫咪进行处理
     const kitties = []
     setKitties(kitties)
+    console.log('populateKitties info')
   }
 
   useEffect(fetchKitties, [api, keyring])
@@ -65,7 +75,7 @@ export default function Kitties (props) {
     <Form style={{ margin: '1em 0' }}>
       <Form.Field style={{ textAlign: 'center' }}>
         <TxButton
-          accountPair={accountPair} label='创建小毛孩' type='SIGNED-TX' setStatus={setStatus}
+          accountPair={accountPair} label='创建小毛孩（等6秒请刷新一下页面,就能看到创建的小猫,react不会)' type='SIGNED-TX' setStatus={setStatus}
           attrs={{
             palletRpc: 'kittiesModule',
             callable: 'create',
